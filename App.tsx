1102ef7e1fda8c74aa2f35ede614a56e0434c54c
medaymenTN <mohamedaymen.ourabi11@gmail.com>
2020-09-18 00:23:50 +0100

    loading font before component render

--- App.tsx
+++ App.tsx
@@ -2,8 +2,18 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
 import Navigation from "./src/routes/navigation.component";
+import * as Font from "expo-font";
 
 export default function App() {
+  /** loading fonts */
+  React.useEffect(() => {
+    (async () =>
+      await Font.loadAsync({
+        Roboto: require("native-base/Fonts/Roboto.ttf"),
+        Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
+      }))();
+  }, []);
+
   return (
     <View style={styles.container}>
       <Navigation />
